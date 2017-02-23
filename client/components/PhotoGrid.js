e2ab181a8c5717fa2918698917f3ae86a6e4b230
Stanislav Pisotskyi <stas27071992@i.ua>
2017-02-23 13:18:51 +0200

    routing setup

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class PhotoGrid extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <div className="photo-grid">
+                    photo grid
+                </div>
+            </div>
+        );
+    }
+}
---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Single extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <div className="single-photo">
+                    single
+                </div>
+            </div>
+        );
+    }
+}
