b070ec846761f391dd2dbeda2422a0453eee140f
Stanislav Pisotskyi <stas27071992@i.ua>
2017-02-23 15:09:47 +0200

    redux store and actions setup

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,29 @@
+//incrementing 'likes' for single post
+export function increment(index)
+{
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    };
+}
+
+//add comment
+export function addComment(postId, author, comment)
+{
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    };
+}
+
+//remove comment
+export function removeComment(postId, i)
+{
+    return {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    };
+}
